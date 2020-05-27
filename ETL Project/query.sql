a8330c53e0c6d7a348b9ce82e07953b5dde4d2b3
Stefan <39097417+stevblag@users.noreply.github.com>
2020-05-26 22:48:46 -0700

    Add files via upload

---
+++ ETL Project/query.sql
@@ -0,0 +1,11 @@
+-- Query the Ford table (6/29/2010 - 6/29/2015)
+SELECT * FROM ford_stock
+
+-- Query the Tesla table (6/29/2010 - 6/29/2015)
+SELECT * FROM tesla_stock
+
+-- Query the VIX table (6/29/2010 - 6/29/2015)
+SELECT * FROM vix_price
+
+-- Query the Combined table
+SELECT * FROM final_merge
